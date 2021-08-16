8c9712374f67f9587dcd04a9d957e1a18962d7b0
Pía Cherni <piaczernyk@gmail.com>
2021-08-16 17:29:08 -0300

    testing silent auth

--- src/views/profile.js
+++ src/views/profile.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../sass/_profile.scss'
 
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 // import { Loading } from "../components";
 
+
+
+
+
 const Profile = () => {
-  const { user } = useAuth0();
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const [userButton, setUserButton] = useState(null);
+  
+  useEffect(() => {
+  const getUserMetadata = async () => {
+    const domain = "dev-amqiay7a.us.auth0.com";
+
+    try {
+      console.log(isAuthenticated);
+      const accessToken = await getAccessTokenSilently({
+        audience: `https://${domain}/api/v2/`,
+        scope: "read:current_user",
+      });
+
+      console.log(isAuthenticated);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  getUserMetadata();
+}, [getAccessTokenSilently, userButton]);
+
+  
+  
+
   // if(!user){
   //   return (
   //     <div>Please sign up or log in</div>
@@ -18,6 +47,7 @@ const Profile = () => {
 
   return (
     <div className='profile'>
+    <button onClick={()=>{setUserButton(true)}}></button>
       <div className="row align-items-center profile-header">
         <div className="col-md-2 mb-3">
           <img
